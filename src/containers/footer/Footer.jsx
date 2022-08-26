d926d4d4a0d5933d4b443be504a9ba6f3e4eb41d
big monkey <shrewdtv@hotmail.com>
2022-08-26 14:07:09 -0400

    created footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import './footer.css';
+import knightlogo from '../../assets/StBernardsKnights.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='chaos__footer section__padding' id='contact'>
+        <div className='chaos__footer-links'>
+            <div className='chaos__footer-links__logo'>
+                <img src={knightlogo} alt="logo" />
+                <p>P.O. Box 604, St. Bernard's Road, Gladstone, NJ 07934</p>
+            </div>
+            <div className='chaos__footer-links__div'>
+                <h4>Social Media</h4>
+                
+                
+                
+                <a href='https://www.instagram.com/teamchaos2458/' target="_blank"><p>Instagram</p></a>
+                <a href='https://www.youtube.com/channel/UCqk76hEIWZlGuEWP22WrCpQ' target="_blank"><p>YouTube</p></a>
+                <a href='https://www.facebook.com/Team-Chaos-2458-103939814302828/' target="_blank"><p>Facebook</p></a>
+            </div> 
+            <div className='chaos__footer-links__div'>
+
+                <h4>Team and Mentor Emails</h4>
+                <a href='mailto:teamchaos2458@gmail.com'><p>teamchaos2458@gmail.com</p></a>
+                <a href='mailto:hkatz@gsbschool.org'><p>hkatz@gsbschool.org</p></a>
+                <a href='mailto:cjohnson@gsbschool.org'><p>cjohnson@gsbschool.org</p></a>
+                <a href='mailto:lgrabowski@gsbschool.org'><p>lgrabowski@gsbschool.org</p></a>
+                <a href='mailto:sbrown@gsbschool.org'><p>sbrown@gsbschool.org</p></a>
+                <a href='mailto:alessandrasanchez214@gmail.com'><p>alessandrasanchez214@gmail.com</p></a>
+            </div> 
+        </div>  
+        <div className='chaos__footer-copyright'>
+            <p>© 2022 Team Chaos 2458. All rights reserved.</p>
+        </div>
+    </div>
   )
 };
 
