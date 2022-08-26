6514ae1168785d5b34df936cd346e0488970c5cf
big monkey <shrewdtv@hotmail.com>
2022-08-26 14:10:08 -0400

    added core values

--- src/containers/whatChaos/WhatChaos.jsx
+++ src/containers/whatChaos/WhatChaos.jsx
@@ -13,7 +13,7 @@ const WhatChaos = () => {
         </div>
         <div className='chaos__whatchaos-container'>
             <Feature title="History" text="Founded in 2008 by a GSB high school physics teacher, Team Chaos has been a catalyst for passion, innovation, and teamwork in the Gladstone community."/>
-            <Feature title="Mission" text="Foster great innovation and togetherness in the STEM community and GSB."/>
+            <Feature title="Mission" text="Foster great innovation and togetherness in the STEM community and GSB. Our core values are Compassion, Integrity, Respect, Courage, and Excellence."/>
             <Feature title="Events" text="I thought about doing a charity ping pong tournament at GSB."/>
         </div>
     </div>
