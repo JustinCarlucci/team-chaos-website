a7becc440e997f5eaa5c2dc5ee92806ed9b50901
big monkey <shrewdtv@hotmail.com>
2022-08-26 14:05:18 -0400

    added cta for sponsors

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='chaos__cta'>
+        <div className='chaos__cta-content'>
+            <p>Want to support the Team?</p>
+            <h3>Register your company to become a sponsor.</h3>
+        </div>
+        <div className="chaos__cta-button">
+            <button type="button">Sponsor Us</button>
+        </div>
+    </div>
   )
 };
 
