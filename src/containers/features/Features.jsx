7b46d20da0b9acbe5ed525eed4f95b1468a29d74
big monkey <shrewdtv@hotmail.com>
2022-08-26 14:01:26 -0400

    Updated gallery colors

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -8,8 +8,9 @@ const Features = () => {
   return (
     <div className='chaos__features section__padding' id='gallery'>
         <div className='chaos__features-heading'>
-            <h1 className='gradient__text'>Team Gallery</h1>
-            <p>Standing at the forefront of STEM education, Team Chaos aims to bring comradery and compassion to the Robotics community. Innovation and leadership are not just goals to us, they are our way of life, despite our "chaotic" spirit.</p>
+            <h1>Team Gallery</h1>
+            <p>Standing at the forefront of STEM education, Team Chaos aims to bring comradery and compassion to the Robotics community. Innovation and leadership are not just goals for us, they're a way of life.</p>
+            <p>Our historically small roster has furthered our collective understanding of the different subjects in STEM. We strive to make a difference in our community through GSB's core values and ironically "chaotic" teamwork.</p>
         </div>
         <div className='chaos__features-slideshow'>
             <Feature title='' text=''/>
