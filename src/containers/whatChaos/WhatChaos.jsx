0cfd8044c8099b2da61d0d22ef6a715fc586ce0b
big monkey <shrewdtv@hotmail.com>
2023-01-18 16:50:55 -0500

    updated events content

--- src/containers/whatChaos/WhatChaos.jsx
+++ src/containers/whatChaos/WhatChaos.jsx
@@ -14,7 +14,7 @@ const WhatChaos = () => {
         <div className='chaos__whatchaos-container'>
             <Feature title="History" text="Founded in 2008 by a GSB high school physics teacher, Team Chaos has been a catalyst for passion, innovation, and teamwork in the Gladstone community."/>
             <Feature title="Mission" text="Foster great innovation and togetherness in the STEM community and GSB. Our core values are Compassion, Integrity, Respect, Courage, and Excellence."/>
-            <Feature title="Events" text="I thought about doing a charity ping pong tournament at GSB."/>
+            <Feature title="Events" text="Our next tournament is at Mount Olive High School from March 10-12th."/>
         </div>
     </div>
   )
